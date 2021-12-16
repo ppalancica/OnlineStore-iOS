228895d4f5cec40a81caf09f3bb417f414c7fde2
System Administrator <root@Pavels-MacBook-Pro.local>
2021-12-17 00:01:53 +0200

    Removed the Main.storyboard file. Renamed ViewController to RootViewController

--- OnlineStore-iOS/RootViewController.swift
+++ OnlineStore-iOS/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  OnlineStore-iOS
 //
 //  Created by Pavel Palancica on 12/16/21.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
