cf8db90ea9b60586c67c49a8e2b9dcae4b5684fc
System Administrator <root@Pavels-MacBook-Pro.local>
2021-12-17 23:55:49 +0200

    Added some groups and structured the Xcode project

--- OnlineStore-iOS/ViewControllers/RootViewController.swift
+++ OnlineStore-iOS/ViewControllers/RootViewController.swift
@@ -12,6 +12,8 @@ class RootViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .lightGray
     }
     
 }
